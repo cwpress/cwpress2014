d412425cc1837b550559f7bdd4667b5ece7be9a0
MrDirby <normhuelsman@mac.com>
2014-07-11 19:28:40 -0400

    edits to home page

--- home-merch.php
+++ home-merch.php
@@ -19,6 +19,7 @@
 	<div class="clear"></div>
 	<div class="retail-divide">OUR OWN LINE OF RETAIL GOods are READILY AVAILABLE online and our brick and mortar STORE</div>
 	<a href="http://compressmerch.com/"><img src="<?php echo site_url(); ?>/wp-content/themes/cwpress2014/images/cwpress-merch.png"></a>
+	<div class="print2live">PRINT TO LIVE <i class="fa fa-instagram"></i> LIVE TO PRINT</div>
 
 	</div>
 </div>
