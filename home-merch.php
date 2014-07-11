467a0203bb529c8bea0da27c1f8e4410c6932747
MrDirby <normhuelsman@mac.com>
2014-07-11 18:49:53 -0400

    home merch section style and content

--- home-merch.php
+++ home-merch.php
@@ -8,6 +8,17 @@
 <div class="merch-wrapper">
 	<div class="merch-content">
 
-	Merch n'at
+	<div class="home-order">
+	<a href="#"><img src="<?php echo site_url(); ?>/wp-content/themes/cwpress2014/images/place-order.svg"></a>
+	</div>
+
+	<div class="tesimonial-container">
+	<?php echo get_new_royalslider(4); ?>
+	</div>
+
+	<div class="clear"></div>
+	<div class="retail-divide">OUR OWN LINE OF RETAIL GOods are READILY AVAILABLE online and our brick and mortar STORE</div>
+	<a href="http://compressmerch.com/"><img src="<?php echo site_url(); ?>/wp-content/themes/cwpress2014/images/cwpress-merch.png"></a>
+
 	</div>
 </div>
