0298df3ac7fd4f562234c8056e1f43bec84d0aaa
MrDirby <normhuelsman@mac.com>
2014-07-14 14:01:05 -0400

    correct paths to js folder

--- header.php
+++ header.php
@@ -30,9 +30,9 @@
 	<![endif]-->
 
 	<!-- easy tabs  -->
-	<script src="<?php echo site_url(); ?>/cwpress/wp-content/themes/cwpress2014/js/jquery-1.11.1.min.js" type="text/javascript"></script>
-	<script src="<?php echo site_url(); ?>/cwpress/wp-content/themes/cwpress2014/js/jquery.hashchange.min.js" type="text/javascript"></script>
-	<script src="<?php echo site_url(); ?>/cwpress/wp-content/themes/cwpress2014/js/jquery.easytabs.js" type="text/javascript"></script>
+	<script src="<?php echo site_url(); ?>/wp-content/themes/cwpress2014/js/jquery-1.11.1.min.js" type="text/javascript"></script>
+	<script src="<?php echo site_url(); ?>/wp-content/themes/cwpress2014/js/jquery.hashchange.min.js" type="text/javascript"></script>
+	<script src="<?php echo site_url(); ?>/wp-content/themes/cwpress2014/js/jquery.easytabs.js" type="text/javascript"></script>
 
 
 	<?php wp_head(); ?>
