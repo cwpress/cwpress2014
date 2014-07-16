c1bb13341dd25594bbf735d307f923baeab37548
MrDirby <normhuelsman@mac.com>
2014-07-16 11:12:03 -0400

    syntax

--- comments.php
+++ comments.php
@@ -15,11 +15,11 @@
  */
 if ( post_password_required() )
 	return;
-if ( have_comments() ) { ?>
+if ( have_comments() ) ?>
 
 	<img src="<?php echo get_stylesheet_directory_uri(); ?>/images/farmer-skulls-comments.svg" class="farmer-comments">
 
-<?php } endif; // have_comments() ?>
+<?php  endif; // have_comments() ?>
 
 <div id="comments" class="comments-area">
 	
