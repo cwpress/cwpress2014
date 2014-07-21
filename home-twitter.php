64c955f01c4b6f336f818a9c03ec07aef26f2076
MrDirby <normhuelsman@mac.com>
2014-07-21 12:14:14 -0400

    home mercy and twitter updates

--- home-twitter.php
+++ home-twitter.php
@@ -15,15 +15,16 @@ Twitter area for the homepage.
 	<div class="twitter-username"><a href="https://twitter.com/cwpress/">@CWPRESS</a></div>
 	
 	<?php echo do_shortcode('[sstfeed]'); ?>
-
+	<script type="text/javascript" src="//platform.twitter.com/widgets.js"></script>
 	<ul>
-		<li><a href="#">reply</a></li>
-		<li><a href="#">retweet</a></li>
-		<li><a href="#">favorite</a></li>
-		<li><a href="#">follow</a></li>
+		<li><a href="https://twitter.com/intent/tweet?screen_name=cwpress">reply</a></li>
+		<li><a href="https://twitter.com/intent/retweet">retweet</a></li>
+		<li><a href="https://twitter.com/intent/favorite">favorite</a></li>
+		<li><a href="https://twitter.com/intent/user?screen_name=cwpress">follow</a></li>
 	</ul>
 
 	<a href="https://twitter.com/cwpress/"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/cw-twitter-icon.png"></a>
 
 	</div>
 </div>
+
