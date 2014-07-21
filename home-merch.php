7621a5d4880096ab572bfe0b2fe05915712a5268
MrDirby <normhuelsman@mac.com>
2014-07-21 10:16:16 -0400

    add linkcious plugin code to front page

--- home-merch.php
+++ home-merch.php
@@ -18,7 +18,38 @@
 
 	<div class="clear"></div>
 	<div class="retail-divide">OUR OWN LINE OF RETAIL GOods are READILY AVAILABLE online and our brick and mortar STORE</div>
-	<a href="http://compressmerch.com/"><img src="<?php echo site_url(); ?>/wp-content/themes/cwpress2014/images/cwpress-merch.png"></a>
+	
+	<!-- Shopify Plugin linkcious - plugin lives on shopify and they give this code -->
+	<div id = "linkcious_ads" style = "display: inline-block;" ></div>
+	<script charset="utf-8" src = "http://www.linkcious.com/linkcious.js"></script>
+	<script>
+	Linkcious.init({
+		settings: {
+			type: 'products',
+			key: '5322f771fd92ad9bc9d231363cd70c5292f4ce20',
+			rows: 1,
+			cols: 4,
+			img_width: 200,
+			img_height: 200,
+			title: true,
+			price: true,
+		},
+		theme: {
+			ads: {
+				color: '#000000',
+				background: 'transparent',
+				font: 'Helvetica Neue',
+				size: 15,
+				title_bold: true,
+				img_border: true,
+				img_border_color: #ECECEC,
+			},
+		}
+	});	
+	</script> <!-- end linkcious -->
+
+
+
 	<div class="print2live">PRINT TO LIVE <i class="fa fa-instagram"></i> LIVE TO PRINT</div>
 
 	</div>
