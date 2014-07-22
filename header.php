77eaa27ea004a9f77e35a9686052969e5a53ad6d
MrDirby <normhuelsman@mac.com>
2014-07-22 10:38:21 -0400

    fix last update

--- header.php
+++ header.php
@@ -31,9 +31,9 @@
 	<![endif]-->
 
 	<!-- easy tabs  -->
-	<script src="<?php echo get_template_directory_uri(); ?>/js/jquery-1.11.1.min.js" type="text/javascript"></script>
-	<script src="<?php echo get_template_directory_uri(); ?>/js/jquery.hashchange.min.js" type="text/javascript"></script>
-	<script src="<?php echo get_template_directory_uri(); ?>/js/jquery.easytabs.js" type="text/javascript"></script>
+	<script src="<?php echo get_stylesheet_directory_uri(); ?>/js/jquery-1.11.1.min.js" type="text/javascript"></script>
+	<script src="<?php echo get_stylesheet_directory_uri(); ?>/js/jquery.hashchange.min.js" type="text/javascript"></script>
+	<script src="<?php echo get_stylesheet_directory_uri(); ?>/js/jquery.easytabs.js" type="text/javascript"></script>
 
 
 	<?php wp_head(); ?>
