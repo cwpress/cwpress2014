e462e7733a8faf32f3d7c0a4c2c0d0e91392ca2b
MrDirby <normhuelsman@mac.com>
2014-09-03 15:50:29 -0400

    switch shirt tabs from text to svg icons

--- page-print.php
+++ page-print.php
@@ -12,6 +12,7 @@
  */
 
 get_header(); ?>
+	<?php get_template_part( 'svg-shirts', 'svg' ); ?>
 
 	<div id="primary" class="content-area">
 		<div id="content" class="site-content" role="main">
@@ -24,11 +25,16 @@ get_header(); ?>
 				<p>(select one)</p>
 				</div>
 			  <ul class='etabs print-tabs'>
-			    <li class='tab'><a href="#shirts">Shirts</a></li>
-			    <li class='tab'><a href="#sweatshirts">Sweatshirts</a></li>
-			    <li class='tab'><a href="#stickers">Stickers</a></li>
-			    <li class='tab'><a href="#posters">Posters</a></li>
-			    <li class='tab'><a href="#questions">Questions</a></li>
+			    <li class='tab'><a href="#shirts">
+			    	<svg class="icon icon-shirt" viewBox="0 0 32 32"><use xlink:href="#icon-shirt"></use></svg></a></li>
+			    <li class='tab'><a href="#sweatshirts">
+			    	<svg class="icon icon-sweatshirt" viewBox="0 0 32 32"><use xlink:href="#icon-sweatshirt"></use></svg></a></li>
+			    <li class='tab'><a href="#stickers">
+			    	<svg class="icon icon-stickers" viewBox="0 0 32 32"><use xlink:href="#icon-stickers"></use></svg></a></li>
+			    <li class='tab'><a href="#posters">
+			    	<svg class="icon icon-posters" viewBox="0 0 32 32"><use xlink:href="#icon-posters"></use></svg></a></li>
+			    <li class='tab'><a href="#questions">
+			    	<svg class="icon icon-questions" viewBox="0 0 32 32"><use xlink:href="#icon-questions"></use></svg></a></li>
 			  </ul>
 			 
 
