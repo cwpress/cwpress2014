ba7d2eb3ebfb44cc3398d15218a963c097de1b75
MrDirby <normhuelsman@mac.com>
2014-09-09 12:32:17 -0400

    sweatshirt icons

--- print-sweatshirts.php
+++ print-sweatshirts.php
@@ -26,11 +26,7 @@ Template for the sweatshirts tab of the print page.
 			<li><a href="#zip-up">
 			<svg class="icon icon-sweatshirt" viewBox="0 0 32 32"><use xlink:href="#icon-sweatshirt"></use></svg></a>Zip Up</a></li>
 			<li><a href="#pullover">
-			<svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
-                   width="100px" height="100px" viewBox="0 0 100 100" enable-background="new 0 0 100 100" xml:space="preserve">
-               <path class="shirt1" d="M99.822,17.266C93.799,12.244,62.069,3.808,62.069,3.808C59.259,8.629,50.02,9.433,50.02,9.433h-0.002
-                  C50.01,9.431,40.78,8.627,37.969,3.808c0,0-31.729,8.436-37.752,13.458L7.043,38.15h13.656l-1.205,59.068h61.05L79.339,38.15h13.654
-                  L99.822,17.266z"/></svg>Pullover</a></li>
+			<svg class="icon icon-sweatshirt" viewBox="0 0 32 32"><use xlink:href="#icon-sweatshirt"></use></svg></a>Pullover</a></li>
 		</ul>
 			<div id="zip-up">
 				<h4>Zip Up</h4>
