f1f7ca9a2efa7b181084676e4b4b102c0039fcee
MrDirby <normhuelsman@mac.com>
2014-09-17 17:33:55 -0400

    print questions update

--- print-questions.php
+++ print-questions.php
@@ -9,7 +9,7 @@ Template for the questions tab of the print page.
 <div class="tab-container-top">
  <div class="print-content-container"> <!-- allow .tab-container-top to stretch full screen and contain content 800 width -->
 	  		<div class="tab-container-top-left">
-			<h2>You have questions, we have answers.</h2>
+			<h2>we love to print. we love solving problems. you have questions, we have answers...</h2>
 			<p><a href="mailto:info@cwpress.com">Email us</a>...</p>
 			</div>
 	    <div class="clear"></div>
